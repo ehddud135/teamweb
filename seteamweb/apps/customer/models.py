37a24de71fd9e02d98c5ae35ac308e5e2df79199
dyhwang <dyhwang@stealien.com>
2024-12-06 16:56:41 +0900

    modify db models

--- seteamweb/apps/customer/models.py
+++ seteamweb/apps/customer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from ..manager.models import Manager
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    manager = models.ForeignKey(Manager, to_field="name", on_delete=models.SET_NULL, null=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        managed = True
+        db_table = 'Customer'
--- seteamweb/apps/manager/models.py
+++ seteamweb/apps/manager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Manager(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=254)
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        managed = True
+        db_table = 'Manager'
