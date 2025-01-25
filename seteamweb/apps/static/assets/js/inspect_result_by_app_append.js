728c81cae82a1534752d724d7adf4d2625c9eca9
dyhwang <dyhwang@stealien.com>
2025-01-25 21:16:19 +0900

    update

--- seteamweb/apps/static/assets/js/inspect_result_by_app_append.js
+++ seteamweb/apps/static/assets/js/inspect_result_by_app_append.js
@@ -7,8 +7,8 @@ const swalWithBootstrapButtons = Swal.mixin({
 });
 
 document.addEventListener('DOMContentLoaded', function (){
-
-    const modal = new bootstrap.Modal(document.getElementById('inspect-result-modal-form'))
+    const modal_form = document.getElementById('inspect-result-modal-form')
+    const modal = new bootstrap.Modal(modal_form)
     const form = document.getElementById('resultAppendForm');
     const btn = document.getElementById('inspection_result_append_btn')
     const customerPicker = document.getElementById('inspection-customer-picker');
@@ -24,67 +24,66 @@ document.addEventListener('DOMContentLoaded', function (){
             } else {
                 document.getElementById("customer-name-label").textContent = `고객사 명 : ${customer_name}`;
                 document.getElementById("customer-name").value = customer_name
-                const package_name = document.getElementById('package_name')
-                const platforms = document.getElementById('platform')
-                platforms.addEventListener('change', (event) =>{
-                    console.log("test")
-                    loadPakcageListByCustomer(customer_name, package_name, event.target.value)
-                })
             }
-            
-            
-
-            form.addEventListener('submit', async function (e) {
-                e.preventDefault();
-                const formData = new FormData(form);
-                try {
-                    const response = await fetch(form.action, {
-                        method: form.method,
-                        headers: {
-                            'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
-                        },
-                        body: formData
-                    });
-                    const data = await response.json();
-                    console.log(data)
-
-                    if (response.ok) {
-                        swalWithBootstrapButtons.fire({
-                            icon: 'success',
-                            title: 'Success alert',
-                            text: 'Your work has been saved',
-                            showConfirmButton: true,
-                            timer: 1500
-                        }).then(() => {
-                            // SweetAlert 종료 후 모달 닫기
-                            const modal = bootstrap.Modal.getInstance(document.getElementById('inspect-result-modal-form'));
-                            if (modal) {
-                                modal.hide();
-                            }
-                        });
-                        console.log('Success:', data);
-                    } else {
-                        swalWithBootstrapButtons.fire(
-                            'Warning alert',
-                            data['error'],
-                            'warning'
-                        )
-                        console.error('Error:', response.statusText);
-                    }
-                } catch (err) {
-                    console.error('Network Error:', err);
-                }
-            });
         } else {
             console.error('Form not found!');
         }
-    })
+    });
+    const checkBoxes = document.querySelector('#option-checkboxes');
+    const package_name = document.getElementById('package_name');
+    const platforms = document.getElementById('platform');
+    platforms.addEventListener('change', (event) =>{
+        loadPakcageListByCustomer(customer_name, package_name, event.target.value)
+        createCheckBoxes(checkBoxes, event.target.value);
+    });
+
+    form.addEventListener('submit', async function (e) {
+        e.preventDefault();
+        const formData = new FormData(form);
+        try {
+            const response = await fetch(form.action, {
+                method: form.method,
+                headers: {
+                    'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+                },
+                body: formData
+            });
+            const data = await response.json();
+            console.log(data)
+
+            if (response.ok) {
+                swalWithBootstrapButtons.fire({
+                    icon: 'success',
+                    title: 'Success alert',
+                    text: 'Your work has been saved',
+                    showConfirmButton: true,
+                    timer: 1500
+                }).then(() => {
+                    // SweetAlert 종료 후 모달 닫기
+                    if (modal) {
+                        modal.hide();
+                        modal_form.querySelector('#resultAppendForm').reset();
+                    }
+                });
+                console.log('Success:', data);
+            } else {
+                swalWithBootstrapButtons.fire(
+                    'Warning alert',
+                    data['error'],
+                    'warning'
+                )
+                console.error('Error:', response.statusText);
+            }
+        } catch (err) {
+            console.error('Network Error:', err);
+        }
+    });
 
 })
 
 async function loadPakcageListByCustomer(name, package_name, platform){
     try {
-        const response = await fetch(`/package-list-by-cusomter-api/${name}/${platform}`)
+        const response = await fetch(`/package-list-by-cusomter-api/${name}/${platform}`);
         const package_list = await response.json();
         package_name.innerHTML = ''
         package_list.forEach(package =>{
@@ -93,9 +92,30 @@ async function loadPakcageListByCustomer(name, package_name, platform){
             option.textContent = package.name;
             package_name.appendChild(option);
         });
-
     } catch (e){
         console.log(e)
     }
 }
 
+function createCheckBoxes(checkBoxes, os){
+    if (os === 'iOS'){
+        options = ['jailbreak_test', 'jailbreak', 'integrity', 'string_encrypt', 'symbol_del']
+    }else if (os === 'android'){
+        options = ['rooting_test', 'rooting', 'integrity', 'emulator', 'obfuscate', 'decompile']
+    }
+
+    options.forEach(option => {
+        const div = document.createElement('div');
+        div.className = "form-check mb-2";
+        const checkbox = document.createElement('input');
+        checkbox.className = "form-check-input";
+        checkbox.type = 'checkbox';
+        checkbox.id = option;
+        checkbox.value = option;
+        checkbox.name = "option";
+        checkbox.checked = false;
+        checkbox.textContent = option
+        div.appendChild(checkbox);
+        checkBoxes.appendChild(div);
+    });
+}
