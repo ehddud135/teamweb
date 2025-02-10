f73a01b3dd021531f8b34a2bd93588667a18bca0
ehddud135 <57941314+ehddud135@users.noreply.github.com>
2025-02-10 18:41:03 +0900

    Update settings.py

--- seteamweb/core/settings.py
+++ seteamweb/core/settings.py
@@ -26,7 +26,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'report_save_path')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('django_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
